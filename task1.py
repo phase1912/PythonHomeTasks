63398db4427d84a8a675a2528de46c8c9c8303de
Bohdan Razhyk <>
2024-09-16 12:34:51 +0200

    fist task has done

---
+++ task1.py
@@ -0,0 +1,24 @@
+﻿from datetime import datetime
+
+def get_days_from_today(date):
+    try:
+        now = datetime.today().date()
+        datetime_object = datetime.strptime(date, "%Y-%m-%d").date()
+    except ValueError:
+        print(f'input data {date} does not match format patern YYYY-MM-DD')
+        return None
+    except:
+        print('general error')
+        return None
+    else:
+        return (now - datetime_object).days
+
+
+date_str = input('Input date in format YYYY-MM-DD: ')
+
+result = get_days_from_today(date_str)
+
+if result is None:
+    print('Not correct input data')
+else:
+    print(result)
