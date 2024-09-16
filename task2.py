1ec7fb23a90af34dbea9bcef8d4045a589b82336
Bohdan Razhyk <>
2024-09-16 14:04:58 +0200

    third task has done

--- task2.py
+++ task2.py
@@ -10,21 +10,21 @@ def get_numbers_ticket(min, max, quantity):
 
         if min < 1:
             print(f'min value must be more than {min}')
-            return None
+            return []
 
         if max > 1000:
             print(f'max value must be less than {max}')
-            return None
+            return []
 
         values = random.sample(range(min, max), quantity)
 
         result_values = sorted(values)
     except ValueError:
         print('not correct input numbers')
-        return None
+        return []
     except:
         print('general error')
-        return None
+        return []
     else:
         return result_values
 
@@ -36,4 +36,4 @@ quantity = input('input quantity of these unique values: ')
 
 result = get_numbers_ticket(min, max, quantity)
 
-print(result)
+print("Ваші лотерейні числа:", result)
---
+++ task3.py
@@ -0,0 +1,36 @@
+﻿import re
+
+# functions
+
+def normalize_phone(phone_number):
+    try:
+        phone_number = re.sub(r"[^0-9+]", "", phone_number)
+
+        if not phone_number.startswith("38") and not phone_number.startswith("+"):
+            phone_number = f"38{phone_number}"
+
+        if "+" not in phone_number:
+            phone_number = f"+{phone_number}"
+
+    except:
+        return ""
+    else:
+        return phone_number
+
+# main code
+
+raw_numbers = [
+    "067\\t123 4567",
+    "(095) 234-5678\\n",
+    "+380 44 123 4567",
+    "380501234567",
+    "    +38(050)123-32-34",
+    "     0503451234",
+    "(050)8889900",
+    "38050-111-22-22",
+    "38050 111 22 11   ",
+]
+
+sanitized_numbers = [normalize_phone(num) for num in raw_numbers]
+
+print("Нормалізовані номери телефонів для SMS-розсилки:", sanitized_numbers)
